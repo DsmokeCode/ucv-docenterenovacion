b28ec12473cbd56455a90dc1f009807c9f1a6d57
Arnold Ramos Calderon <aramosc.16@gmail.com>
2022-03-21 23:56:08 -0500

    actualicacion de arrays

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,12 +6,7 @@ interface Food {
   value: string;
   viewValue: string;
 }
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -20,7 +15,28 @@ export interface PeriodicElement {
 export class HomeComponent {
   columnas: string[] = ['codigo', 'descripcion', 'precio'];
 
-  datos: Articulo[] = [];
+  datos: Articulo[] = [
+    { codigo: 1, descripcion: 'Hydrogen', precio: 1.0079 },
+    { codigo: 2, descripcion: 'Helium', precio: 4.0026 },
+    { codigo: 3, descripcion: 'Lithium', precio: 6.941 },
+    { codigo: 4, descripcion: 'Beryllium', precio: 9.0122 },
+    { codigo: 5, descripcion: 'Boron', precio: 10.811 },
+    { codigo: 6, descripcion: 'Carbon', precio: 12.0107 },
+    { codigo: 7, descripcion: 'Nitrogen', precio: 14.0067 },
+    { codigo: 8, descripcion: 'Oxygen', precio: 15.9994 },
+    { codigo: 9, descripcion: 'Fluorine', precio: 18.9984 },
+    { codigo: 10, descripcion: 'Neon', precio: 20.1797 },
+    { codigo: 11, descripcion: 'Sodium', precio: 22.9897 },
+    { codigo: 12, descripcion: 'Magnesium', precio: 24.305 },
+    { codigo: 13, descripcion: 'Aluminum', precio: 26.9815 },
+    { codigo: 14, descripcion: 'Silicon', precio: 28.0855 },
+    { codigo: 15, descripcion: 'Phosphorus', precio: 30.9738 },
+    { codigo: 16, descripcion: 'Sulfur', precio: 32.065 },
+    { codigo: 17, descripcion: 'Chlorine', precio: 35.453 },
+    { codigo: 18, descripcion: 'Argon', precio: 39.948 },
+    { codigo: 19, descripcion: 'Potassium', precio: 39.0983 },
+    { codigo: 20, descripcion: 'Calcium', precio: 40.078 },
+  ];
   dataSource: any;
   foods: Food[] = [
     { value: '0', viewValue: 'Pregrado presencial' },
@@ -46,13 +62,17 @@ export class HomeComponent {
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
 
   ngOnInit() {
-    for (let x = 1; x <= 100; x++)
-      this.datos.push(
-        new Articulo(x, `artículo ${x}`, Math.trunc(Math.random() * 1000))
-      );
     this.dataSource = new MatTableDataSource<Articulo>(this.datos);
     this.dataSource.paginator = this.paginator;
   }
+}
+export class PeriodicElement {
+  constructor(
+    public name: string,
+    public position: number,
+    public weight: number,
+    public symbol: string
+  ) {}
 }
 export class Articulo {
   constructor(
@@ -61,25 +81,3 @@ export class Articulo {
     public precio: number
   ) {}
 }
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
-];
